f240caee51b90ff13dded753ced171ecc11e3389
Harukaru <92298736+Harukaru1@users.noreply.github.com>
2023-12-27 23:01:16 +0700

    Edit header.

--- public/main.js
+++ public/main.js
@@ -4,8 +4,13 @@ const togtopmenuIcon = document.getElementById('top-menu-toggle');
 document.addEventListener('click', (e) => {
   if (togtopmenuIcon.contains(e.target)) {
     //click the menu toggle
+    topMenu.classList.toggle('top-menu-expanded');
     topMenu.classList.toggle('hidden');
   } else {
     //Click outside the menu toggle
+    if (topMenu.classList.contains('top-menu-expanded')) {
+      topMenu.classList.remove('top-menu-expanded');
+      topMenu.classList.add('hidden');
+    }
   }
 });
